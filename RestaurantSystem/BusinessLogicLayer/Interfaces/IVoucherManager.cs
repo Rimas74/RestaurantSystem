b8dce2f073e263810daf3ae5e744dae8dbe0a503
Rimas74 <gintilas.rimantas@gmail.com>
2024-02-08 21:54:20 +0200

    added string GetSenderEmail()

--- RestaurantSystem/BusinessLogicLayer/Interfaces/IVoucherManager.cs
+++ RestaurantSystem/BusinessLogicLayer/Interfaces/IVoucherManager.cs
@@ -8,5 +8,6 @@ namespace RestaurantSystem.BusinessLogicLayer.Interfaces
         IVoucher CreateRestaurantVoucher(Order order);
         void PrintVoucher(IVoucher voucher);
         void SendVoucherByEmail(IVoucher voucher, string emailAddress);
+        string GetSenderEmail();
         }
     }
